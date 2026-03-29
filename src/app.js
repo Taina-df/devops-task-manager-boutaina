71bc8b37578fa93511c4e4ca88324dd15df759b2
Taina-df <difa.boutaina@etu.uae.ac.ma>
2026-03-29 18:21:30 +0100

    Fix double app.listen issue

--- src/app.js
+++ src/app.js
@@ -1,19 +1,7 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const PORT = 3000;
 
-const tasksRouter = require('./routes/tasks');
-
-app.get('/', (req, res) => {
-  res.json({ message: "Task Manager API running (Lab2)" });
-
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
-
-app.use('/tasks', tasksRouter);
-app.listen(3000, ()=> console.log("API running on port 3000"));
-
-if (require.main === module) {
-  app.listen(3000, () => console.log("API running on port 3000"));
-}
 
 module.exports = app;
